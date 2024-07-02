9b15794556414bea024ce8311d172a8df00bffd4
Dhruv Joshi <whodhruvjoshi@gmail.com>
2024-07-02 21:21:13 +0530

    Create LED3.ino

---
+++ LED3.ino
@@ -0,0 +1,14 @@
+// Glow the LEDs in following order
+// 3,7 delay – 2,6 delay – 1,5 delay – 0,4 delay
+// 2. Glow the LEDs in following order
+// 0,4 delay – 1,5 delay – 2,6 delay – 3,7 delay
+
+// 3. Glow 0 to 7 LEDs with delay and 7 to 0 LEDs with
+// delay
+// 4. Glow 0,2,1,3,4,6,5,7 LEDs with delay
+
+// 5. Glow the LEDs in following order
+// 0,1 delay – 6,7, delay – 2,3, delay – 4,5, delay
+// 6. Glow the LEDs in following order
+// 4,5 delay – 2,3, delay – 6,7, delay – 0,1, delay
+
